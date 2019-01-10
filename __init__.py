80d9f14da8fb8e3bfdf085414b4339edb7839a0c
Adam Hoover <ashoover@gmail.com>
2019-01-10 00:39:07 -0600

    Login System
    Finished up the login/registration system.  Need to add a "reset password" option still.

--- __init__.py
+++ __init__.py
@@ -1,20 +1,31 @@
 from flask import Flask, render_template, flash, request, url_for, redirect, session
 from np_config_list import HostList
 import time
+import logging
 from dbconnect import connection
 from wtforms import Form, validators, PasswordField, StringField
 from passlib.hash import sha256_crypt
 from pymysql import escape_string as thwart
 import gc
 
-
 # Random Variables
 HOST_DICT = HostList()
 time_now = time.strftime("%H:%M %m-%d-%Y")
 
+####
 # Settings
+####
+
+# Logging Settings
+logging.basicConfig(level=logging.DEBUG,
+                    filename="netpop.log",
+                    format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+# Flask Settings
 app = Flask(__name__)
 app.secret_key = 'my_secret_key'
+app.debug = True
 
 ###
 # Routes
@@ -25,8 +36,8 @@ app.secret_key = 'my_secret_key'
 def homepage():
     return render_template("main.html")
 
-# Monitor Page
 
+# Monitor Page
 @app.route('/monitor/')
 def monitor():
     try:
@@ -34,8 +45,8 @@ def monitor():
     except Exception as e:
         return render_template("error.html", error=e)
 
-# Settings page
 
+# Settings page
 @app.route('/settings/')
 def settings():
     try:
@@ -43,28 +54,55 @@ def settings():
     except Exception as e:
         return render_template("error.html", error=e)
 
-# Login/Register Page
+
+### Login/Register Page
+
+
+# Login Page
+@app.route('/login/', methods=["GET", "POST"])
+def login_page():
+
+
+    try:
+        error = ""
+        c, conn = connection()
+
+        if request.method == "POST":
+            data = c.execute("SELECT * FROM users WHERE username = %s", thwart(request.form['username'],))
+            data = c.fetchone()[5]
+
+            if sha256_crypt.verify(request.form['password'], data):
+                session['logged_in'] = True
+                session['username'] = request.form['username']
+                flash("You are now logged in!")
+
+                return redirect(url_for('monitor'))
+
+        gc.collect()
+
+        return render_template("login.html")
+
+    except Exception as e:
+        flash(f"Login Error.  Try Again!")
+        return render_template("login.html")
+
+
+    # Registration Page
+
 
 class RegistrationForm(Form):
     username = StringField('Username', [validators.Length(min=4, max=20)])
     email = StringField('Email Address', [validators.Length(min=6, max=50)])
     password = PasswordField('Password', [validators.DataRequired(),
-                                            validators.EqualTo('confirm', message='Passwords must match.')])
-    confirm = PasswordField('Corfirm Password')
+                                          validators.EqualTo('confirm', message='Passwords must match.')])
+    confirm = PasswordField('Confirm Password')
 
 
-@app.route('/login/', methods=['GET','POST'])
-def login(): 
-    try:
-        return render_template("login.html")
-    except Exception as e:
-        return render_template("error.html", error=e)
-
-@app.route('/register/', methods=['GET','POST'])
-def register_page(): 
+@app.route('/register/', methods=['GET', 'POST'])
+def register_page():
     try:
         form = RegistrationForm(request.form)
-        c,conn = connection()
+        c, conn = connection()
 
         if request.method == "POST" and form.validate():
             username = form.username.data
@@ -74,15 +112,17 @@ def register_page():
             x = c.execute("SELECT * FROM users WHERE username = %s", (username,))
 
             if int(x) > 0:
-                flash("Username taken. Try another!")
+                flash("That username is taken. Try another!")
                 return render_template("register.html", form=form)
 
             else:
-                c.execute("INSERT INTO users (username, password, email) VALUES (%s, %s, %s)",(thwart(username), thwart(password), thwart(email)))
+                c.execute("INSERT INTO users (username, password, email) VALUES (%s, %s, %s)", (thwart(username),
+                                                                                                thwart(password),
+                                                                                                thwart(email)))
 
                 conn.commit()
 
-                flash("Thanks for Registering")
+                flash(f"Thanks for registering {username}!")
 
                 c.close()
                 conn.close()
@@ -98,21 +138,25 @@ def register_page():
     except Exception as e:
         return render_template("error.html", error=e)
 
+
 # Error Handling
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html")
 
+
 @app.errorhandler(405)
 def page_not_found(e):
     return render_template("405.html")
+
 
 @app.errorhandler(500)
 def server_issue(e):
     return render_template("500.html", error=e)
 
-# Main App
 
+# Main App
 if __name__ == '__main__':
     app.run(debug=True)
