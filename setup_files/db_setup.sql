747997ab2deb8108572d9a88c4800ce46c50879e
Adam Hoover <ashoover@gmail.com>
2019-01-16 03:22:18 -0600

    Updated DB Schema File
    SQL setup queries are now in setup_files/db_setup.sql

---
+++ setup_files/db_setup.sql
@@ -0,0 +1,62 @@
+-- Users Table Creation
+CREATE TABLE users (
+    uid INT(11) AUTO_INCREMENT PRIMARY KEY, 
+    firstname VARCHAR(30), 
+    lastname VARCHAR(30), 
+    username VARCHAR(20), 
+    mobilenum VARCHAR(11),
+    password VARCHAR(100), 
+    email VARCHAR(50), 
+    settings VARCHAR(3000), 
+    rank INT(3), 
+    lastlogin VARCHAR(20),
+    last_invalid_login VARCHAR(20)
+)
+ENGINE=MyISAM
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci;
+
+
+-- Endpoints Table Creation
+CREATE TABLE endpoints (
+    uid INT(11) AUTO_INCREMENT PRIMARY KEY, 
+    hostname VARCHAR(50),
+    ip VARCHAR(15),
+    zip INT(10),
+    last_check VARCHAR(24),
+    enabled BOOL,
+    endabled_date VARCHAR(24),
+    disabled_date VARCHAR(24),
+    creation_date VARCHAR(24)
+)
+ENGINE=MyISAM
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci;
+
+
+-- Enpoint Logs Table Creation
+CREATE TABLE endpoint_log (
+	uid INT(11) AUTO_INCREMENT PRIMARY KEY,
+	check_time varchar(24) NULL,
+	response_time varchar(10) NULL,
+	endpoint_alive BOOL NULL,
+	notice_sent BOOL NULL,
+	notice_time_sent varchar(24) NULL,
+	CONSTRAINT endpoint_log_pk PRIMARY KEY (uid)
+)
+ENGINE=MyISAM
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci;
+
+
+-- Contact Logs Table Creation
+CREATE TABLE cont_log (
+	uid INTEGER(11) NOT NULL AUTO_INCREMENT,
+	recipient varchar(100) NULL,
+	date_sent varchar(100) NULL,
+	message_type varchar(100) NULL,
+	CONSTRAINT cont_log_pk PRIMARY KEY (uid)
+)
+ENGINE=MyISAM
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci;
